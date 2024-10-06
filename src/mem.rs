8071d8ea5de131987f76f85ab2dd77857623522f
skouliou <imad.eddine.nezzar@gmail.com>
2024-10-06 14:59:21 +0100

    Refactoring

--- src/mem.rs
+++ src/mem.rs
@@ -16,9 +16,25 @@ impl Memory {
         todo!()
     }
 
-    /// open file in binary mode and load it into memory
-    pub(crate) fn load_file(path: &str) -> std::io::Result<Self> {
-        todo!()
+    /// open file load it into memory
+    pub(crate) fn load_file(path: &str) -> Result<Memory, Box<dyn Error>> {
+        let file = File::open(path)?;
+        let file_size = file.metadata()?.size() as usize;
+
+        // file is larger than what memory can handle
+        if file_size > 0xFFFF {
+            return Err(Box::new(NotEnoughMemory(file_size)));
+        }
+
+        let mut file = BufReader::new(file);
+        let mut buffer = Vec::with_capacity(file_size);
+
+        file.read_to_end(&mut buffer)?;
+
+        let mem = Memory {
+            mem: Box::from(buffer),
+        };
+        Ok(mem)
     }
 
     /// get the content held in given memory address
@@ -31,5 +47,19 @@ impl Memory {
         todo!()
     }
 }
+
+#[derive(Debug)]
+pub(crate) struct NotEnoughMemory(usize);
+
+impl Display for NotEnoughMemory {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "NotEnoughMemory: file size ({})is larger than memory size",
+            self.0
+        )
+    }
+}
+impl Error for NotEnoughMemory {}
 
 pub(crate) struct InvalidAddress;
